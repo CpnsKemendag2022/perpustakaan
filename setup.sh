39dade04401b9cf69ac2bab7ea3ee3fdf1d3fb9c
CpnsKemendag2022 <102676302+CpnsKemendag2022@users.noreply.github.com>
2022-04-10 15:12:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"cpnskemendag.2022@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS = false\n\
